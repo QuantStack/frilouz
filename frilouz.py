ffb0fdad45e76862388f885561c2065e20f4f773
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2021-03-06 00:14:20 +0100

    Rework error boundaries detection
    Use some kind of state machine based on DEDENT, COLON and INDENT token type to
    compute error boundaries.
    
    Document that in the code \o/

--- frilouz.py
+++ frilouz.py
@@ -11,39 +11,77 @@ def _indent(line):
             break
     return indent
 
-def _isdefcls(tok):
-    if tok.exact_type != token.NAME:
-        return False
-    return tok.string == 'def' or tok.string == 'class'
+def _keep_indent(toks):
+    for tok in reversed(toks):
+        if tok.exact_type in (token.INDENT, token.DEDENT, token.NEWLINE):
+            continue
+        return tok.exact_type == token.COLON
+    return False
 
 def _build_safe_code(code, error_line):
+    '''
+    Return a new version of `code` with the lines causing the error at
+    `error_line` replaced by `pass`.
+    Tries to be smart about which lines to prune
+    '''
+
+    # We have two recovery algorithm:
+    #
+    # 1. if the error happens at an indented line, we just prune all tokens
+    #    until next DEDENT token
+    #
+    # 2. if the error happens on a non-indented line, we prune all tokens until
+    #    next NEXLINE token
     all_lines = code.split('\n')
     tmp = io.BytesIO(code.encode())
-    lastdeforcls = 0
-    lastdeforclsindent = ''
-    error_recovery = False
+    currindent = ''
+    stop_on_tokens = ()
+    prev_toks = []
     for tok in tokenize.tokenize(tmp.readline):
-        if tok.exact_type == token.ENDMARKER:
-            continue
-        if error_recovery:
-            if _isdefcls(tok):
-                error_recovery = False
-                lastdeforcls = tok.start[0] - 1
-                lastdeforclsindent = _indent(all_lines[lastdeforcls])
-                break
-            elif tok.exact_type != token.DEDENT:
-                all_lines[tok.start[0] - 1] = lastdeforclsindent + 'pass'
-        elif _isdefcls(tok):
-            lastdeforcls = tok.start[0] - 1
-            lastdeforclsindent = _indent(all_lines[lastdeforcls])
-        elif tok.start[0] == error_line:
+        # Pass in error erasure mode
+        if tok.start[0] == error_line and not stop_on_tokens:
             # In case we find a def / class that holds extra indent information
             # we would like to use.
             if tok.exact_type == token.INDENT:
-                continue
-            error_recovery = True
-            for i in range(lastdeforcls, error_line):
-                all_lines[i] = lastdeforclsindent + 'pass'
+                stop_on_tokens = token.DEDENT, token.ENDMARKER
+            else:
+                stop_on_tokens = token.NEWLINE, token.ENDMARKER
+        # or perform the actual error erasure
+        elif tok.exact_type in stop_on_tokens:
+            # if we're right after a colon, keep current line indentation
+            if _keep_indent(prev_toks):
+                indent = _indent(all_lines[error_line - 1])
+            # otherwise use computed indentation
+            else:
+                indent = currindent
+
+            # compute error boundary
+            if tok.exact_type == token.DEDENT:
+                correct_line = tok.start[0] - 1
+            elif tok.exact_type == token.ENDMARKER:
+                correct_line = len(all_lines)
+            else:
+                correct_line = tok.start[0]
+
+            # actual erasure
+            for i in range(error_line - 1, correct_line):
+                all_lines[i] = indent + 'pass'
+
+            break
+
+        # register indentation level and previous tokens
+        # as long as we're not in error mode
+        elif not stop_on_tokens:
+            if tok.exact_type == token.INDENT:
+                currindent += tok.string
+            elif tok.exact_type == token.DEDENT:
+                currindent = current[:-len(tok.string)]
+
+            # try to keep token history small
+            if tok.exact_type == token.COLON:
+                prev_toks.clear()
+            prev_toks.append(tok)
+
     return '\n'.join(all_lines)
 
 def parse(parser, code, *args, **kwargs):
